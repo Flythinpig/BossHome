6c1e9b6a2ce8c553cd3f0ed224950fd976322886
jingf <1106201458@qq.com>
2015-12-07 13:33:25 +0800

    A new file

---
+++ src.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+int removeDuplicates( int A[], int n )
+{
+	if( n == 0 ) return 0;
+
+	int p = 0;
+	for( int i = 0; i < n - 1; i++ )
+	{
+		if( A[i] != A[i + 1] )
+		{
+			A[p++] = A[i];
+		}
+	}
+
+	A[p] = A[n - 1];
+
+	return p + 1;
+
+	/*if( n == 0 ) return 0;
+
+	int index = 0;
+	for( int i = 1; i < n; i++ )
+	{
+		if( A[i] != A[index] )
+			A[++index] = A[i];
+	}
+
+	return index + 1;*/
+}
+
+int main()
+{
+	int n;
+	while( cin >> n )
+	{
+		int *arr = new int[n];
+		for( int i = 0; i < n; i++ )
+			cin >> arr[i];
+
+		int x = removeDuplicates( arr, n );
+
+		cout << x << endl;
+		for( int i = 0; i < x; i++ )
+			cout << arr[i] << " ";
+
+		cout << endl;
+		delete arr;
+		arr = NULL;
+	}
+	return 0;
+}
